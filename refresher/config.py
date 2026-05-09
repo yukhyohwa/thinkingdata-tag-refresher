bc52fc0dfd2ee864d15b999aed04d86ba6840fd7
shawnlu <shawnlu91@gmail.com>
2026-05-09 13:38:40 +0800

    chore: Update thinkingdata tag refresher script

--- refresher/config.py
+++ refresher/config.py
@@ -11,16 +11,14 @@ load_dotenv()
 TA_URL      = os.getenv("TA_URL",  "http://8.211.141.76:8993/")
 TA_USER     = os.getenv("TA_USER", "")
 TA_PASS     = os.getenv("TA_PASS", "")
-TAG_URL     = os.getenv("TAG_URL", "http://8.211.141.76:8993/#/tag/tag/1?currentProjectId=16")
+TAG_URL     = os.getenv("TAG_URL", "http://8.211.141.76:8993/#/tag/tag/1?currentProjectId=13")
 SESSION_DIR = os.path.abspath(os.getenv("SESSION_DIR", "./ta_session"))
 
 # Tags to refresh, in order
 TAGS_TO_REFRESH = [
-    "fixed_regdate",
-    "fixed_affcode",
     "fixed_os",
     "fixed_country",
-    "fixed_freeamount",
+    "fixed_regdate",
 ]
 
 # Milliseconds to wait after each successful refresh before moving to the next tag
